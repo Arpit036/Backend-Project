7f4af4c94f68d767966a970d8f10b1e935d4b45b
Arpit  Kumar Verma <132037701+Arpit036@users.noreply.github.com>
2025-05-20 02:31:06 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -12,6 +12,14 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log(`Server is runing at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MOGO DB CONNECTION FAILED !!", err);
+})
 
 
 
