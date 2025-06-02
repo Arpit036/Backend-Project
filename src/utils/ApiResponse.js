59b64a233b473ada0424028de3487625ae3b2613
Arpit  Kumar Verma <132037701+Arpit036@users.noreply.github.com>
2025-06-02 11:05:16 +0530

    completed lect. 13

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,7 +1,10 @@
 class ApiResponse {
-    constructor(statusCode, data, message = "success")
+    constructor(statusCode, data, message = "success"){
     this.statusCode = statusCode
     this.data = data
     this.message = message
     this.sucess = statusCode < 400
 }
+}
+
+export {ApiResponse}
