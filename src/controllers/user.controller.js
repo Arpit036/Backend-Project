4fbbd077f9bd76776a9166a8abfb3e57f1d1b336
Arpit  Kumar Verma <132037701+Arpit036@users.noreply.github.com>
2025-06-06 23:41:27 +0530

    add mongodb aggregate pipeline code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -301,6 +301,80 @@ const updateUserCoverImage  = asyncHandler(async(req, res) => {
   )
 })
 
+
+const getUserChannelProfile = asyncHandler(async(req, res) => {
+  const {username} = req.params
+
+   if(!username?. trim()){
+    throw new ApirError(400, "username is missing")
+   }
+
+   const channel = await User.aggregate([
+    {
+       $match : {
+        username : username?.toLowerCase()
+       }
+    },
+    {
+      $lookup: {
+        from : "subscriptions",
+        localField: '_id',
+        foreignField: "channel",
+        as : "subscribers"
+
+      }
+    },
+    {
+      $lookup: {
+        from : "subscriptions",
+        localField: '_id',
+        foreignField: "subsciber",
+        as : "subscribedTo"
+      }
+    },
+      {
+        $addFields: {
+          subscribersCount : {
+            $size: "$subscribers"
+          },
+          channelsSubscribedToCount: {
+            $size: "$subscribedTo"
+          },
+          isSubscribed: {
+            $cond: {
+              if: {$in: [req.user?._id, "$usbscribers.subscriber"]},
+              then: true,
+              else: false
+            }
+          }
+        }
+      },
+      {
+        $project:{
+          fullName: 1,
+          username: 1,
+          subscribersCount: 1,
+          channelsSubscribedToCount: 1,
+          isSubscribed: 1,
+          avatar:1,
+          coverImage:1,
+          email:1
+        }
+      }
+
+   ])
+
+   if(!channel?.length){
+    throw new ApiError(404,"Channel doesn't exists")
+   }
+   return res
+   .status(200)
+   .json(
+    new ApiResponse(200, channel[0], "User channel fetched successfully")
+   )
+ })
+
+
 export {
   registerUser,
   loginUser,
@@ -310,5 +384,7 @@ export {
   getCurrentUser,
   updateAccountDetails,
   updateUserAvatar,
-  updateUserCoverImage
+  updateUserCoverImage,
+  getUserChannelProfile
+
      };
